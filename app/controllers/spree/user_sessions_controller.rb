fb8d051199e91533c907de98e3063a75c5f04af2
Ashish Singh <aitashish173@gmail.com>
2017-03-08 13:58:46 +0530

    Fixed user login/logout issue

--- app/controllers/spree/user_sessions_controller.rb
+++ app/controllers/spree/user_sessions_controller.rb
@@ -12,17 +12,18 @@ class Spree::UserSessionsController < Devise::SessionsController
   include Spree::Core::ControllerHelpers::Store
   skip_before_action :verify_authenticity_token, if: -> {request.format.json?}
 
+  prepend_before_action :allow_params_authentication!, only: :create
+
   # ssl_required :new, :create, :destroy, :update
   # ssl_allowed :login_bar
 
   def create
-    authenticate_spree_user!
+    user = Spree::User.find_by_email(params[:email])
+    # Warden authentication
+    self.resource = warden.authenticate!(auth_options)
+
     if spree_user_signed_in?
       respond_to do |format|
-        format.html {
-          flash[:success] = Spree.t(:logged_in_succesfully)
-          redirect_back_or_default(after_sign_in_path_for(spree_current_user))
-        }
         format.json {
           @user = spree_current_user
           @order = current_order
@@ -36,15 +37,16 @@ class Spree::UserSessionsController < Devise::SessionsController
       end
     else
       respond_to do |format|
-        format.html {
-          flash.now[:error] = t('devise.failure.invalid')
-          render :new
-        }
         format.json {
           render json: { error: t('devise.failure.invalid') }, status: :unprocessable_entity, layout: false
         }
       end
     end
+  end
+
+  def auth_options
+    # This is to login spree_user(Spree::User)
+    { :scope => :spree_user }
   end
 
   def destroy
