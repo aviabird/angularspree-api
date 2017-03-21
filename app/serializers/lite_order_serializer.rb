68f34e4c29600123ae037791e1ed61a459c3a642
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-21 07:19:03 +0530

    fixing prod assets

--- app/serializers/lite_order_serializer.rb
+++ app/serializers/lite_order_serializer.rb
@@ -1,7 +1,7 @@
 class LiteOrderSerializer < BaseSerializer
   attributes(
   	:number, :payment_state, :completed_at, :state, :total,
-  	:shipment_state, :created_at, :currency, :line_items
+  	:shipment_state, :created_at, :updated_at, :currency, :line_items
   )
 
   has_many :line_items, serializer: LiteLineItemSerializer
