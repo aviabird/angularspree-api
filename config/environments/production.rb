12cc3bc4f01fa9a0d85fd7a0a0363413dd2fc6b5
Pankaj Rawat <pankajrawat19sept@gmail.com>
2017-03-22 08:06:48 +0530

    heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Paperclip.options.merge!(:command_path => "/usr/bin")
 end
