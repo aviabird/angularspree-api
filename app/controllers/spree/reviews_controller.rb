d389cf94fb4cacb37dcf13d742f3f18dd3f57e43
gopalshimpi <gopalshimpi@gmail.com>
2018-06-06 11:02:06 +0530

    Added Product Review & Rating  API (#24)
    Ticket : https://www.pivotaltracker.com/story/show/158024093
    ## Why?
    **Feature** : We need have to API for creating product review & rating. 
    
    ## This change addresses the need by:
    
    As we have added extension for  [product review ](https://github.com/spree-contrib/spree_reviews). We have override controllers which will render the `json` for api. The API is   `<host>/products/product_id/reviews` using post method, which will take `params` as 
    ```
    [Delivers #158024093]

---
+++ app/controllers/spree/reviews_controller.rb
@@ -0,0 +1,50 @@
+class Spree::ReviewsController < Spree::StoreController
+  helper Spree::BaseHelper
+  before_action :load_product, only: [:index, :new, :create]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
+  def index
+    @approved_reviews = Spree::Review.approved.where(product: @product)
+  end
+
+  def new
+    @review = Spree::Review.new(product: @product)
+    authorize! :create, @review
+  end
+
+  # save if all ok
+  def create
+    params[:review][:rating].sub!(/\s*[^0-9]*\z/, '') unless params[:review][:rating].blank?
+
+    @review = Spree::Review.new(review_params)
+    @review.product = @product
+    @review.user = spree_current_user if spree_user_signed_in?
+    @review.ip_address = request.remote_ip
+    @review.locale = I18n.locale.to_s if Spree::Reviews::Config[:track_locale]
+
+    authorize! :create, @review
+    if @review.save
+      # flash[:notice] = Spree.t(:review_successfully_submitted)
+      message = Spree.t(:review_successfully_submitted)
+      # redirect_to spree.product_path(@product)
+      render json: { message: message }
+    else
+      message = "Unable to submit the review"
+      render json: { error: message }
+    end
+  end
+
+  private
+
+  def load_product
+    @product = Spree::Product.friendly.find(params[:product_id])
+  end
+
+  def permitted_review_attributes
+    [:rating, :title, :review, :name, :show_identifier]
+  end
+
+  def review_params
+    params.require(:review).permit(permitted_review_attributes)
+  end
+end
