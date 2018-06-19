6becb104bd16875eea53ca409dae0ecc7636d34b
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-06-19 11:05:02 +0530

    bug fixes for order creation and updates

--- app/controllers/spree/api/base_controller_decorator.rb
+++ app/controllers/spree/api/base_controller_decorator.rb
@@ -1,4 +1,5 @@
 Spree::Api::BaseController.class_eval do
+  skip_before_action :verify_authenticity_token
   def api_key
     request.headers['Auth-Token'] || params[:token]
   end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -18,30 +20,30 @@ module AngularspreeApi
 
     config.to_prepare do
       # Load application's model / class decorators
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
       # Load application's view overrides
-      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+      Dir.glob(File.join(File.dirname(__FILE__), '../app/overrides/*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
 
-    config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
       allow do
         origins '*'
 
         resource '/cors',
-          :headers => :any,
-          :methods => [:post],
-          :credentials => false,
-          :max_age => 0
+                 headers: :any,
+                 methods: [:post],
+                 credentials: false,
+                 max_age: 0
 
         resource '*',
-          :headers => :any,
-          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
-          :max_age => 0
+                 headers: :any,
+                 methods: %i[get post delete put patch options head],
+                 max_age: 0
       end
     end
   end
