4ea172fd661b47ee6184e8ac7bcb495617f6d16a
Gopal B Shimpi <gopalshimpi@gmail.com>
2018-07-24 23:58:11 +0530

    User signup fixes (#48)
    Why?
    
    Bug
    New user unable to signup using signup form.
    
    This change addresses the need by:
    
    New user is able to signup.
    
    [delivers #159241359]

--- app/controllers/api/accounts_controller.rb
+++ app/controllers/api/accounts_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::AccountsController < BaseController
-  before_action :check_authorization, except: %i[handle_payment]
+  before_action :check_authorization, except: %i[create]
 
   def serialization_scope
     current_order
