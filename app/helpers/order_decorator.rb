9025e3ef26b733738cbeab8d4df2fbf5b0f294c1
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-09-05 01:23:11 +0530

    Fixed issue with order persistance by merging guest order with user order

---
+++ app/helpers/order_decorator.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+Spree::Core::ControllerHelpers::Order.module_eval do
+  def set_current_order
+    return unless try_spree_current_user && current_order
+    
+    # Find order by guest token and assign him logged in user when signs in
+    Spree::Order
+      .find_by(guest_token: request.headers['Order-Token'])
+      &.associate_user!(try_spree_current_user)
+
+    incomplete_orders = try_spree_current_user
+                        .orders
+                        .incomplete
+                        .where('id != ?', current_order.id)
+
+    incomplete_orders
+      .each { |order| current_order.merge!(order, try_spree_current_user) }
+  end
+end
