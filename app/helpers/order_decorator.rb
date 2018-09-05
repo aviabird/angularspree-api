03d16805ff0237fb1b5778aa34d60805096e46cb
Pankaj Rawat <pankajrawat19sept@gmail.com>
2018-09-05 11:11:40 +0530

    bug fixed with current order in order_decorator

--- app/helpers/order_decorator.rb
+++ app/helpers/order_decorator.rb
@@ -5,16 +5,20 @@ Spree::Core::ControllerHelpers::Order.module_eval do
     return unless try_spree_current_user
 
     # Find order by guest token and assign him logged in user when signs in
-    Spree::Order
-      .find_by(guest_token: request.headers['Guest-Order-Token'])
-      &.associate_user!(try_spree_current_user)
+    guest_order = Spree::Order
+                  .find_by(guest_token: request.headers['Guest-Order-Token'])
+                  &.associate_user!(try_spree_current_user)
+
+    user_order = current_order || guest_order
+
+    return unless user_order
 
     incomplete_orders = try_spree_current_user
                         .orders
                         .incomplete
-                        .where('id != ?', current_order.id)
+                        .where('id != ?', user_order.id)
 
     incomplete_orders
-      .each { |order| current_order.merge!(order, try_spree_current_user) }
+      .each { |order| user_order.merge!(order, try_spree_current_user) }
   end
 end
